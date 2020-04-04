8ad55c59a5167b9be509ef792ffb72ed57829d2e
CodyJReed <cody2j2reed@gmail.com>
2020-04-04 15:05:35 -0400

    Added type guard and began creating condition for sorting a string

--- build/index.js
+++ build/index.js
@@ -5,15 +5,19 @@ var Sorter = /** @class */ (function () {
     }
     Sorter.prototype.sort = function () {
         var length = this.collection.length;
+        // If collection is an array of numbers
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
             }
         }
+        // If collection is an array of strings
     };
     return Sorter;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
+    // If collection is an array of numbers
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
       }
     }
+
+    // If collection is an array of strings
   }
 }
 
