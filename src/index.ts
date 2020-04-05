6f816c06bc68136b97fa00eb4eb00288ab416d87
CodyJReed <cody2j2reed@gmail.com>
2020-04-05 10:16:22 -0400

    Added LinkedList.ts and CharactersCollection.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([3, 150, -7, 8]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([3, -150, -7, 8]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Cody");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
