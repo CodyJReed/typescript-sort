18a782b3e5a602f2127408621b3a8d1d2194a1a1
CodyJReed <cody2j2reed@gmail.com>
2020-04-05 11:10:02 -0400

    Refactor Sorter class as abstarct, and apply as parent to all subs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([3, -150, -7, 8]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("Cody");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection("Cody");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(450);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(-44);
+linkedList.sort();
+linkedList.print();
